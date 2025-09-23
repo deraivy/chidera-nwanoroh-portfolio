a9ba9d2b66cf890d337656ccc328c0cce358b719
ChideraN <nwanorohfavoured@gmail.com>
2025-09-23 23:10:52 +0100

    added project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  nitro: {
+    preset: "netlify", // Use the Netlify preset if needed
+  },
+  routeRules: {
+    "/": { prerender: true },
+    //"/blog/**": { isr: 60 } // revalidate every 60 seconds, for example
+  },
   css: ["./styles/main.css"],
-  plugins: ["./plugins/aos.client.js"]
-})
+  plugins: ["./plugins/aos.client.js"],
+});
